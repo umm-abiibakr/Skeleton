42fe3351d367274d17e0b11ef9b8c59afc0aa267
p2694787 <p2694787@my365.dmu.ac.uk>
2024-05-16 15:09:19 +0100

    customer updated

---
+++ Testing3/testcustomer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomer AnCustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(AnCustomer);
+        }
+    }
+}
