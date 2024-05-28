b82fbf497ccd515c97a41ea4d706e47b7cf05b4a
P2755642 <P2755642@GWHXX472WIN008W>
2024-05-28 19:42:07 +0100

    hello i created a test after 8 weeks

---
+++ Testing5/tstOrderitems.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstOrderitems
+    {
+        public void InstanceOk()
+
+        { //create an instance of the class we want to 
+            clsorderitems Anorderitems = new clsorderitems();
+            //test to see that it exists
+            Assert.IsNotNull(Anorderitems);
+        }
+    }
+}
