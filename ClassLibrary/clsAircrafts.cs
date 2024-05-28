1827d4945d701b93d9a533c30145e6a146a017ee
Sanya Rathod <P2710950@my365.dmu.ac.uk>
2024-05-28 19:10:22 +0100

    properties of table

---
+++ ClassLibrary/clsAircrafts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsAircrafts
+    {
+        public clsAircrafts()
+        {
+        }
+
+        public int AircraftID { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int Quantity { get; set; }
+        public bool Availability { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public string AircraftName { get; set; }
+    }
+}
